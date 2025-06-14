aae4b8e3e30a0a6381cbba53d20f78ceef440bb7
pxlpkr <pixelparkour@parkkila.net>
2025-06-13 17:02:23 -0700

    attempt to get info from mal

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const MAL_CLIENT_ID = "2277fe3d95ad07465b541b0d7627822d";
 
-document.getElementById("submitButton").addEventListener("click", (e) => {
-    const USERNAME = document.getElementById("usernameInput").textContent;
-    main(USERNAME)
-});
+document.addEventListener("DOMContentLoaded", main);
 
-function main(username) {
+function main() {
+    document.getElementById("submitButton").addEventListener("click", () => {
+        const USERNAME = document.getElementById("usernameInput").textContent;
+        submit(USERNAME);
+    });
+}
+
+function submit(username) {
     const MAL_URL = `https://api.myanimelist.net/v2/users/${username}/animelist`;
     const REQ_HEADER = new Headers();
     REQ_HEADER.set('X-MAL-CLIENT-ID', MAL_CLIENT_ID);
